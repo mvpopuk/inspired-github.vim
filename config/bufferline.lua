825a8f6543f6041b09158d3a7d9d72ca06952d55
Marian Pop <mvpopuk@gmail.com>
2022-03-14 23:37:28 +0200

    initial commit

---
+++ config/bufferline.lua
@@ -0,0 +1,71 @@
+require('bufferline').setup {
+  options = {
+    indicator_icon = ' ',
+    numbers = "buffer_id",
+    diagnostics = "nvim-cmp",
+    diagnostics_update_in_insert = true,
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+    local icon = level:match("error") and " " or " "
+    return " " .. icon .. count
+    end,
+    show_buffer_close_icons = true,
+    show_close_icon = false,
+    offsets = {
+      {
+        filetype = 'nerdtree',
+        text = '  File Explorer',
+        highlight = 'Directory',
+        text_align = 'left',
+      },
+    },
+
+    modified_icon = ' ',
+    separator_style = 'thin',
+
+    custom_areas = {
+      left = function()
+        return {
+          { text = '    ', guifg = '#029632',  },
+        }
+      end,
+    },
+  },
+ -- Highlight for Inspired Github
+    highlights = {
+        background = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+            guifg = { attribute = 'fg', highlight = 'LineNr' },
+        },
+        fill = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+            guifg = { attribute = 'fg', highlight = 'EndOfBuffer' },
+        },
+        separator = {
+            guifg = { attribute = 'fg', highlight = 'EndOfBuffer' },
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+        },
+        close_button = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+            guifg = { attribute = 'fg', highlight = 'LineNr' },
+        },
+        close_button_visible = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+            guifg = { attribute = 'fg', highlight = 'LineNr' },
+        },
+        close_button_selected = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+            guifg = { attribute = 'fg', highlight = 'LineNr' },
+        },
+        buffer_visible = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+            guifg = { attribute = 'fg', highlight = 'LineNr' },
+        },
+        modified = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+        },
+        modified_visible = {
+            guibg = { attribute = 'bg', highlight = 'EndOfBuffer' },
+        },
+
+    },   
+}
---
+++ config/lualine.lua
@@ -0,0 +1,121 @@
+-- -- Inspired Github Colors
+local colors = {
+  red = '#ca1243',
+  grey = '#f5f5f5',
+  light_grey = '#979BAC',
+  black = '#383a42',
+  white = '#ffffff',
+  transparent = '#ffffff',
+  light_green = '#83a598',
+  orange = '#fe8019',
+  green = '#8ec07c',
+  yellow = '#f8eec7',
+  cyan = '#489FC1',
+}
+
+local inspired_github = {
+  normal = {
+    a = { fg = colors.white, bg = colors.red },
+    b = { fg = colors.black,  bg = colors.grey },
+    c = { fg = colors.light_grey, bg = colors.white },
+    z = { fg = colors.white, bg = colors.black },
+  },
+  insert = { a = { fg = colors.black, bg = colors.yellow } },
+  visual = { a = { fg = colors.white, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.green } },
+}
+
+local empty = require('lualine.component'):extend()
+function empty:draw(default_highlight)
+  self.status = ''
+  self.applied_separator = ''
+  self:apply_highlights(default_highlight)
+  self:apply_section_separators()
+  return self.status
+end
+
+-- Put proper separators and gaps between components in sections
+local function process_sections(sections)
+  for name, section in pairs(sections) do
+    local left = name:sub(9, 10) < 'x'
+    for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
+      table.insert(section, pos * 2, { empty, color = { fg = colors.white, bg = colors.transparent } })
+    end
+    for id, comp in ipairs(section) do
+      if type(comp) ~= 'table' then
+        comp = { comp }
+        section[id] = comp
+      end
+      comp.separator = left and { right = '' } or { left = '' }
+    -- if you're using iTerm with a text line height more than 100, use the separators below instead
+      -- comp.separator = left and { right = '' } or { left = '' }
+    end
+  end
+  return sections
+end
+
+local function search_result()
+  if vim.v.hlsearch == 0 then
+    return ''
+  end
+  local last_search = vim.fn.getreg('/')
+  if not last_search or last_search == '' then
+    return ''
+  end
+  local searchcount = vim.fn.searchcount { maxcount = 9999 }
+  return last_search .. '(' .. searchcount.current .. '/' .. searchcount.total .. ')'
+end
+
+local function modified()
+  if vim.bo.modified then
+    return '+'
+  elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+    return '-'
+  end
+  return ''
+end
+
+require('lualine').setup {
+  options = {
+    theme = inspired_github,
+    component_separators = '',
+    section_separators = { left = '', right = '' },
+    -- if you're using iTerm with a text line height more than 100, use the separators below instead
+    -- section_separators = { left = '', right = ''},
+  },
+  -- process_sections
+  sections = process_sections {
+    lualine_a = { 'mode' },
+    lualine_b = {
+      'branch',  
+      { 'filename', file_status = false, path = 3 },
+      {
+        'diagnostics',
+        source = { 'intelephense' },
+        sections = { 'error' },
+        diagnostics_color = { error = { bg = colors.red, fg = colors.white } },
+      },
+      {
+        'diagnostics',
+        source = { 'intelephense' },
+        sections = { 'warn' },
+        diagnostics_color = { warn = { bg = colors.orange, fg = colors.white } },
+      },
+      {
+        'diagnostics',
+        source = { 'intelephense' },
+        sections = { 'hint' },
+        diagnostics_color = { warn = { bg = colors.orange, fg = colors.white } },
+      },
+      { modified, color = { bg = colors.yellow } },
+    },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = { search_result, 'filetype' },
+    lualine_z = { '%l:%c', '%p%%/%L' },
+  },
+  inactive_sections = {
+    lualine_c = { '%f %y %m' },
+    lualine_x = {},
+  },
+}
